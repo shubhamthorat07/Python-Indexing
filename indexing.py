337bffff9ccf3d2deab7a45ea766ae8601cebef6
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-08-05 14:01:29 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,20 @@
+s = "Python"
+
+print(type(s))
+print(id(s))
+
+s = "Javascript"
+print(id(s))
+
+
+s1 = "Hello World"
+print(s1[0])
+print(s1[1])
+print(s1[2])
+print(s1[3])
+print(s1[4])
+print(s1[5])
+
+print(s1[-1])
+print(s1[-2])
+print(s1[0], s1[1], s1[2], s1[3], s1[4])
